e841699b6fb6110994b770594cfe458a6c896738
Emil Ahlbäck <e.ahlback@gmail.com>
2014-10-08 13:43:27 +0300

    Initial commit

---
+++ test/models/test_model_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class TestModelTest < ActiveSupport::TestCase
+  test "this shall fail" do
+    assert false
+  end
+
+  test "this shall pass" do
+    assert true
+  end
+end
