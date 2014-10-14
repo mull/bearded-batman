9fb1fd2a79c563308898e42a4df33d6e3ac67561
Emil Ahlbäck <e.ahlback@gmail.com>
2014-10-14 15:25:37 +0300

    raven & intercom and whatnot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def welcome
+    @@last_id ||= 0
+    @@last_id += 1
+    @user = OpenStruct.new(id: @@last_id, email: Faker::Internet.email, name: Faker::Name.name)
+  end
 end
---
+++ config/initializers/raven.rb
@@ -0,0 +1,5 @@
+require 'raven'
+
+Raven.configure do |config|
+  config.dsn = 'http://225cd747829c45918c5d0c7312feb004:74c4ffdaac044b53bbba561f5c3f87be@localhost:3003/2'
+end
